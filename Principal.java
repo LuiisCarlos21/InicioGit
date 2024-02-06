7a7f083ba36c003ca7494bea620efe47e59f7fca
LuiisCarlos21 <1623041@alu.murciaeduca.es>
2024-02-06 08:53:58 +0100

    Principal.java modificado

--- Principal.java
+++ Principal.java
@@ -1,5 +1,9 @@
+import java.util.Date;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!!");
+
+		Date fecha = new Date();
+		System.out.println(fecha);
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # InicioGit
 Repositorio para la primera practica de Git de entornos de desarrollo
+
+## Principal.java
+Porgrama que imprime un "Hola mundo" y muestra la fecha y la hora actual.
